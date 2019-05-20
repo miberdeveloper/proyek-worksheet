0e8599b6b05cd49fbeb832035955a46389e2ee4a
Grandis Ardana A F <jazzmedia998@gmail.com>
2019-05-20 09:55:16 +0700

    grand : menambahkan sidebar

--- config/menu.php
+++ config/menu.php
@@ -6,6 +6,33 @@
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
           </a>
         </li>
+        <li class="treeview <?= $page == "personalia" ? "active" : "" ?>">
+        <!-- diatas aktif default active  -->
+          <a href="#">
+            <i class="fa fa-users"></i> <span>Personalia</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="<?= $page == "personalia" && $statusMenu == "home" ? "active" : "" ?>"><a href="index.php?page=personalia&page2=home"><i class="fa fa-home"></i> Home</a></li>
+            <li class="<?= $page == "personalia" && $statusMenu == "admin" ? "active" : "" ?>"><a href="index.php?page=personalia&page2=admin"><i class="fa fa-user"></i> Admin Site</a></li>
+          </ul>
+        </li>
+        <li class="treeview <?= $page == "keuangan" ? "active" : "" ?>">
+        <!-- diatas aktif default active  -->
+          <a href="#">
+            <i class="fa fa-money"></i> <span>Keuangan</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li class="<?= $page == "keuangan" && $statusMenu == "home" ? "active" : "" ?>"><a href="index.php?page=keuangan&page2=home"><i class="fa fa-home"></i> Home</a></li>
+            <li class="<?= $page == "keuangan" && $statusMenu == "admin" ? "active" : "" ?>"><a href="index.php?page=keuangan&page2=admin"><i class="fa fa-user"></i> Admin Site</a></li>
+          </ul>
+        </li>
+        <li>
         <li class="treeview <?= $page == "pergudangan" ? "active" : "" ?>">
         <!-- diatas aktif default active  -->
           <a href="#">
@@ -32,6 +59,7 @@
             <li class="<?= $page == "ecommerce" && $statusMenu == "admin" ? "active" : "" ?>"><a href="index.php?page=ecommerce&page2=admin"><i class="fa fa-users"></i> Admin Site</a></li>
           </ul>
         </li>
+      <ul>
         <li>
           <a href="#">
             <i class="fa fa-th"></i> <span>Widgets</span>
